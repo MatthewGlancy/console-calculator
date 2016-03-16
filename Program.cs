2335df3ed331fe0b8fc2318841209796885de4eb
Matthew Glancy <ukmatty73@gmail.com>
2016-03-16 14:36:06 +0000

    Added comments for readability
    Multiple comments added on variables, and some lines, for increased
    readability

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            string sNum1;
-            string sNum2;
-            string op;
-            double fNum1;
-            double fNum2;
-            bool repeat;
-            double result;
-            string sRepeat;
+            string sNum1;       //string version of number (required for input)
+            string sNum2;       //as above
+            string op;          //operation
+            double fNum1;       //floating point of number
+            double fNum2;       //as above
+            bool repeat;        //bool to decide whether various loops should repeat
+            double result;      //floating point answer
+            string sRepeat;     //string of input to decide whether calculator will be used again
 
             Console.WriteLine("Welcome to the calculator.");
             Console.WriteLine("");
@@ -27,10 +27,10 @@ namespace Calculator
 
                 do {
                     sNum1 = Console.ReadLine();
-                    repeat = !(double.TryParse(sNum1, out fNum1));
+                    repeat = !(double.TryParse(sNum1, out fNum1)); // if input is a number, it will continue, if not then it will ask to re-input data
                     if (repeat)
                     {
-                        Console.WriteLine("Your input was not a recognised number, please try again.");
+                        Console.WriteLine("Your input was not a recognised number, please try again."); //Basic user feedback
                     }
 
                 } while (repeat);
@@ -39,10 +39,10 @@ namespace Calculator
 
                 do {
                     sNum2 = Console.ReadLine();
-                    repeat = !(double.TryParse(sNum2, out fNum2));
+                    repeat = !(double.TryParse(sNum2, out fNum2)); // if input is a number, it will continue, if not then it will ask to re-input data
                     if (repeat)
                     {
-                        Console.WriteLine("Your input was not a recognised number, please try again.");
+                        Console.WriteLine("Your input was not a recognised number, please try again."); //Basic user feedback
                     }
 
                 } while (repeat);
@@ -51,40 +51,40 @@ namespace Calculator
 
                 do {
                     op = Console.ReadLine();
-                    repeat = !(op.Equals("+") || op.Equals("-") || op.Equals("*") || op.Equals("/"));
+                    repeat = !(op.Equals("+") || op.Equals("-") || op.Equals("*") || op.Equals("/")); // if input is an operator, it will continue, if not then it will ask to re-input data
                     if (repeat)
                     {
-                        Console.WriteLine("Your input was not a recognised operation, please try again.");
+                        Console.WriteLine("Your input was not a recognised operation, please try again."); //Basic user feedback
                     }
 
                 } while (repeat);
 
-                if (op.Equals("+"))
+                if (op.Equals("+")) 
                 {
                     result = fNum1 + fNum2;
-                    Console.WriteLine("{0} + {1} = {2}", sNum1, sNum2, result);
+                    Console.WriteLine("{0} + {1} = {2}", sNum1, sNum2, result); // Tells the user the result of their calculation
                 }
 
                 if (op.Equals("-"))
                 {
                     result = fNum1 - fNum2;
-                    Console.WriteLine("{0} - {1} = {2}", sNum1, sNum2, result);
+                    Console.WriteLine("{0} - {1} = {2}", sNum1, sNum2, result); // As above
                 }
 
                 if (op.Equals("*"))
                 {
                     result = fNum1 * fNum2;
-                    Console.WriteLine("{0} * {1} = {2}", sNum1, sNum2, result);
+                    Console.WriteLine("{0} * {1} = {2}", sNum1, sNum2, result); // As above
                 }
 
                 if (op.Equals("/"))
                 {
                     result = fNum1 / fNum2;
-                    Console.WriteLine("{0} / {1} = {2}", sNum1, sNum2, result);
+                    Console.WriteLine("{0} / {1} = {2}", sNum1, sNum2, result); // As above
                 }
 
                 Console.WriteLine("");
-                Console.WriteLine("Would you like to use the calculator again? (y/n)");
+                Console.WriteLine("Would you like to use the calculator again? (y/n)"); // Allows the user to repeat the calculator
 
                 sRepeat = Console.ReadLine();
                 repeat = (sRepeat.Equals("y") || sRepeat.Equals("Y"));
